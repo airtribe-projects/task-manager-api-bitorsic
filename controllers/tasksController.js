632a513aad078121d206aa6485c07a9743244ebe
bitorsic <bitorsic@gmail.com>
2025-06-17 11:41:10 +0530

    step 2 done

---
+++ controllers/tasksController.js
@@ -0,0 +1,100 @@
+// trying to use map, as I read all crud operations are most efficient on this
+// could have used objects, but read about prototype pollution, and really wanted to try this
+let tasks = new Map();
+let currentId = 1;
+
+exports.getAllTasks = (req, res) => {
+  let result = [];
+  
+  for (const [key, value] of tasks) {
+    result.push({ id: key, ...value });
+  }
+
+  return res.status(200).send(result);
+};
+
+exports.getTaskById = (req, res) => {
+  const id = Number(req.params.id);
+  const task = tasks.get(id);
+
+  if (!task) {
+    return res.status(404).send({
+      message: `Task with id ${id} not found`,
+    });
+  }
+
+  res.status(200).send({ id, ...task})
+};
+
+exports.createTask = (req, res) => {
+  const { title, description, completed } = req.body;
+  let incorrectFields = [];
+
+  if (typeof title !== "string" || title === "") incorrectFields.push("title");
+  if (typeof description !== "string" || description === "") incorrectFields.push("description");
+  if (typeof completed !== "boolean") incorrectFields.push("completed");
+
+  if (incorrectFields.length !== 0) {
+    return res.status(400).send({
+      message: `Incorrect / missing fields: ${incorrectFields.join(", ")}`,
+    })
+  }
+
+  tasks.set(currentId, { title, description, completed });
+
+  return res.status(201).send({ message: `Task successfully created with id ${currentId++}` });
+};
+
+exports.updateTask = (req, res) => {
+  const id = Number(req.params.id);
+  const task = tasks.get(id);
+
+  if (!task) {
+    return res.status(404).send({
+      message: `Task with id ${id} not found`,
+    });
+  }
+
+  const { title, description, completed } = req.body;
+  let updatedFields = []
+
+  if (typeof title === "string" && title !== "") {
+    task.title = title;
+    updatedFields.push("title");
+  }
+
+  if (typeof description === "string" && description !== "") {
+    task.description = description;
+    updatedFields.push("description");
+  }
+
+  if (typeof completed === "boolean") {
+    task.completed = completed;
+    updatedFields.push("completed");
+  }
+
+  if (updatedFields.length === 0) {
+    return res.status(400).send({
+      message: `Could not update task due to incorrect request body format`,
+    });
+  }
+
+  tasks.set(id, task);
+
+  return res.status(200).send({
+    message: `Successfully updated fields for id ${id}: ${updatedFields.join(", ")}`,
+  });
+};
+
+exports.deleteTask = (req, res) => {
+  const id = Number(req.params.id);
+  const isDeleted = tasks.delete(id);
+
+  if (!isDeleted) {
+    return res.status(404).send({
+      message: `Task with id ${id} not found`,
+    });
+  }
+
+  return res.status(200).send({ message: `Task with id ${id} deleted successfully` });
+};
---
+++ routes/tasksRoute.js
@@ -0,0 +1,10 @@
+const router = require('express').Router();
+const controller = require('../controllers/tasksController');
+
+router.get("/", controller.getAllTasks);
+router.get("/:id", controller.getTaskById);
+router.post("/", controller.createTask);
+router.put("/:id", controller.updateTask);
+router.delete("/:id", controller.deleteTask);
+
+module.exports = router;
